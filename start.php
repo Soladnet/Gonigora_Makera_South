0711bb8ce7b6dd82f8d7bdffa5461c84192ca296
Alkali Ibrahim <alkalibrahim@gmail.com>
2012-09-03 02:39:19 +0100

    start.php

---
+++ start.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html>
+<head>
+  <title>
+    Getting Started
+  </title>
+  <link rel="shortcut icon" href="favicon.ico" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />  
+  <!--- http://bit.ly/NfpVMY -->
+        <!--[if IE]>
+        <link rel="stylesheet" href="css/main.css" />
+        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <link rel="stylesheet" media="screen and (min-device-width: 1024px)" href="css/main.css" />
+        <link rel='stylesheet' media="screen and (max-device-width: 1023px)" href="css/medium.css" />
+
+      </head>
+      <body>
+        <div class="container">
+
+          <div id="nav2" class="nav2_gradient">
+            <span id="logo">
+              <a href="http://gossout.com" title="Logo"><img src="images/logo_text_s.png" alt="Logo"></a>
+            </span>
+
+          </div>
+
+          <div class="center_div width800" >
+            <div class="inner_wrappper box_shadow8 center_div" >
+             
+             <div >
+              
+               <div id="about">
+                 <h1>Lets Get Started </h1><hr/>
+                 <div>
+                 <br/>
+                   <h4>1.  Subscribe/Join Communities<hr/></h4>
+                   <div> <img src="images/start_community.png"/>
+                    <span>
+                      <p> Subscribe to relevant communities to get updates on your timeline. Subscribe to any number of communities, you can only join one community at a time, and that's the community you can post in at the moment. You can join any of your subscribed communities at any time. Also, check the communities page to see detailed information on all Gossout communities. </p>
+                    </span>
+                  </div>
+                  <br/>
+                  <h4>2.  Add Friends<hr/></h4>
+                   <div> <img src="images/start_friends.png"/>
+                    <span>
+                      <p> Friends would be suggested to you automatically based on your subscribed communities, add friends so that you can exchange Private Messages, Winks and tweaks </p>
+                    </span>
+                  </div>
+                  
+                 <br/>
+                   <h4>3.  Timeline <hr/></h4>
+                   <div> <img src="images/start_TL.png"/>
+                    <span>
+                      <p> The timeline shows you all the posts from communities you've subscribed. You can also post images to the timeline.</p>
+                    </span>
+                  </div>
+                  
+                  <br/>
+                  <h4>4.  Group Chat<hr/></h4>
+                   <div> <img src="images/start_chat.png"/>
+                    <span>
+                      <p> The Group Chat allows you to chat with all members of the community you joined that are online </p>
+                    </span>
+                  </div>
+                  
+                  <br/>
+                   <h4>5.  Notifications <hr/></h4>
+                   <div> <img src="images/start_notifications.png"/>
+                    <span>
+                      <p> Check all your notifications here, including new posts in your community, Winks, Tweaks, Friend Requests and Private Messages </p>
+                    </span>
+                  </div>
+                  
+                  
+                  
+                 
+               </div>
+             </div>
+             <?php include_once("footer.php"); ?>
+           </div>        
+         </div>
+       </div>
+     </div>       
+   </body>
+   </html>
