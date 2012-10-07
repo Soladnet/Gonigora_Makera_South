a0bdfa1c2aadc8ebe19cc1f5068b0148ded8afe6
Abdulrasheed <soladnet@hotmail.com>
2012-10-07 03:08:42 +0100

    with capcha

--- executecommand.php
+++ executecommand.php
@@ -132,14 +132,16 @@ function manageSession() {
 
 // log user in function
 function login($username, $password, $rem = false) {
-    $user = clean($username);
-    $pass = clean($password);
+    $user = $username;
+    $pass = $password;
 
     //convert password to md5
     $pass = md5($pass);
 
     // check if the user id and password combination exist in database
     $sql = "SELECT l.id, l.email, l.activated,p.dateJoined,  p.firstname, p.lastname, p.gender, p.dob,p.relationship_status,p.phone,p.url,p.bio,p.favquote,p.location,p.likes,p.dislikes,p.works,uc.community_id,c.name,c.category,ar.role FROM user_login_details AS l JOIN user_personal_info AS p ON p.email = l.email LEFT JOIN user_comm as uc on l.id = uc.user_id LEFT JOIN community as c on c.id = uc.community_id LEFT JOIN admin_role as ar ON l.id = ar.admin_user_id WHERE l.email = '$user' AND l.password = '$pass'";
+//    echo $sql;
+//    exit;
     $result = mysql_query($sql);
     $arr = array();
     //if match is equal to 1 there is a match
@@ -206,7 +208,7 @@ function login($username, $password, $rem = false) {
         exit;
     } else {
         // login failed save error to a session
-        $_SESSION['err']['status'] = 'Sorry, wrong username or password';
+        $_SESSION['err']['status'] = 'Sorry, wrong username or password ';
         header('Location: login.php');
         exit;
     }
@@ -511,7 +513,7 @@ function showPostAndComment($userId, $all = 0, $from = 0, $withPost_id = 0, $low
         }
     }
 
-    $limit = "Limit $lowlimit,5";
+    $limit = "Limit $lowlimit,15";
 
     $postValue = "";
     $postSql = "SELECT p.id,p.post,c.name,p.community_id,p.sender_id,p.time,s.`lastname`,s.`firstname`,cp.`250x250`,cp.`original` FROM `post` as p JOIN user_personal_info as s on p.sender_id=s.id JOIN community_subscribers as cs on (cs.user=$userId and cs.`community_id`=p.`community_id`) JOIN community as c on cs.`community_id`=c.id LEFT JOIN community_pix AS cp ON p.id = cp.post_id $where order by p.id desc $limit";
@@ -595,10 +597,64 @@ function sendPost($userId, $community, $comm, $text, $senderFullname) {
         $id = mysql_insert_id();
         $sql = "SELECT c.`community_id`, concat(u.`firstname`,' ',u.lastname) as fullname,u.email FROM `community_subscribers` as c JOIN user_personal_info as u ON c.user=u.id WHERE c.`community_id`=$community";
         $result = mysql_query($sql);
-        if(mysql_num_rows($result)>0){
-            while($row = mysql_fetch_array($result)){
-                
+        $email = trim(strip_tags("post+notification@gossout.com"));
+
+        $full_name = 'Gossout';
+        $from_mail = $full_name . '<' . $email . '>';
+        $message = htmlentities($text);
+
+        // set here
+        $subject = "$senderFullname post to $comm";
+        $to = 'soladnet@gmail.com';
+        if (mysql_num_rows($result) > 0) {
+            while ($row = mysql_fetch_array($result)) {
+                $html = '<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <!--        <link rel="stylesheet" media="screen and (min-device-width: 1024px)" href="css/main.css" />-->
+        <style> a {text-decoration: none;} ol,ul {    list-style: none;} h1,h2,h3,h4,h5,h6 {font-weight: normal; color: #333;font-family: "Avant Garde", Avantgarde, "Century Gothic", CenturyGothic, "AppleGothic", sans-serif;} hr {margin: .3em 0;    width: 100%;    height: 1px;    border-width:0;    color: #ddd;background-color: #ddd;} span {} img {border: none;padding: .2em;    max-width: 100%;} .inner_wrappper {display: inline-block;padding: .5em;background: #fafafa;width: 100%;}
+            .nav2_gradient {background-color: #f3f3f3; background-image: -webkit-gradient(linear,left top,left bottom,from(#f3f3f3),to(#dad9d7));background-image: -webkit-linear-gradient(top,#f3f3f3,#dad9d7);background-image: -moz-linear-gradient(top,#f3f3f3,#dad9d7);background-image: -ms-linear-gradient(top,#f3f3f3,#dad9d7);background-image: -o-linear-gradient(top,#f3f3f3,#dad9d7);background-image: linear-gradient(to bottom,#f3f3f3,#dad9d7);}#logo a img {padding: .5em;} .index_fnx{background-color:#FAFAFA; height: 140px; border: 1px solid #F4F4F4; margin-top: 2px;}
+            .friend_index{background: url(http://gossout.com/images/image-friend.png) no-repeat left top!important;}
+            /*            .community_index{background: url(images/image-community.png) no-repeat left top!important;}*/
+            #column1,#column2 {display: inline-block;width: 49.5%;vertical-align: top;}
+            #column1 {text-align: left;}.box_shadow8 {-webkit-box-shadow: 0 0 8px 0 #999;box-shadow: 0 0 8px 0 #999;}
+            .center_div { margin: 0px auto 0;} .width800{        width: 800px;} .clear {clear: both;} #nav2 {border-bottom: 1px solid #717373;} #nav2 div{} .index_fnx .fnx{text-align:center;font-size: 1em;font-weight: bold;} .fnx_detail{font-size: .85em;}#footer{    padding: 5px 10px 5px 10px; margin: 0 auto; } #footer a{    color:#333; padding: 0 .2em;} #footer a:hover{    color:#A6CC8B;} #footer li {    float: left;}</style>
+    </head>
+    <body> 
+        <div>
+            <div class="center_div width800">
+                <div class="inner_wrappper box_shadow8 center_div ">
+                    <div id="column1" style="width: 100%">
+                        <div  class="community_index index_fnx" > 
+                            <span> <h1 class="fnx"><img src="http://gossout.com/images/G.png" /> ' . $comm . '<hr> </h1>
+                                <p class="fnx_detail"><img src="http://www.gossout.com/' . $_SESSION['auth']['image50x50'] . '" align="left"/><strong>' . $senderFullname . '</strong><br/>' . $message . '</p>
+                            </span>
+                        </div>
+
+                    </div>
+                    <div id="footer" class="p_name">
+                        <hr>
+                        <table cellspacing="5px">
+                            <tr >
+                                <td colspan="3" style="font-size: 13px" font-family: \'Segoe UI\',sans-serif;> This message was sent to $to.
+                            </td>
+                        </tr>
+                    </table>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
+';
             }
+            $headers = "Reply-To: $from_mail \r\n"."From:" . $from_mail . "\r\n" ."X-Mailer: PHP/" . phpversion();
+            $headers .= 'MIME-Version: 1.0' . "\r\n";
+            $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+            @mail($to, $subject, $html, $headers);
         }
         alertGossbag($userId, $id, $community, "$senderFullname post to " . $comm);
         $arr['id'] = $id;
--- testemail.php
+++ testemail.php
@@ -1,64 +1,42 @@
-<?php
-session_start();
-include 'executecommand.php';;
-connect();
-//
-//sendGetExternalData("http://9gist.com/gossoutFile/gossoutFunction.php", $post_data);
-////sendEmail("solanet@gmail.com", "info@gossout.com", "Gossout Community", "Welcome to Gossout Community", $message);
-?>
-<!DOCTYPE HTML>
-<html> 
-    <head> 
-        <title>Loader</title> 
-        <style type="text/css">
-            div#container {
-                width:500px;
-                height:500px;
-                overflow:auto;
-            }
-        </style> 
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script> 
-        <script type="text/javascript">
- 
-            $(document).ready(function(){
-                $.ajax({
-                    url: 'index.php',
-                    success: function(data){
-                        $('#load').html(data); // Load data into a <div> as HTML
-//                        alert('The page was loaded!');
-                    }
-                });
-            });
-        </script>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <!--        <link rel="stylesheet" media="screen and (min-device-width: 1024px)" href="css/main.css" />-->
+        <style> a {text-decoration: none;} ol,ul {    list-style: none;} h1,h2,h3,h4,h5,h6 {font-weight: normal; color: #333;font-family: "Avant Garde", Avantgarde, "Century Gothic", CenturyGothic, "AppleGothic", sans-serif;} hr {margin: .3em 0;    width: 100%;    height: 1px;    border-width:0;    color: #ddd;background-color: #ddd;} span {} img {border: none;padding: .2em;    max-width: 100%;} .inner_wrappper {display: inline-block;padding: .5em;background: #fafafa;width: 100%;}
+            .nav2_gradient {background-color: #f3f3f3; background-image: -webkit-gradient(linear,left top,left bottom,from(#f3f3f3),to(#dad9d7));background-image: -webkit-linear-gradient(top,#f3f3f3,#dad9d7);background-image: -moz-linear-gradient(top,#f3f3f3,#dad9d7);background-image: -ms-linear-gradient(top,#f3f3f3,#dad9d7);background-image: -o-linear-gradient(top,#f3f3f3,#dad9d7);background-image: linear-gradient(to bottom,#f3f3f3,#dad9d7);}#logo a img {padding: .5em;} .index_fnx{background-color:#FAFAFA; height: 140px; border: 1px solid #F4F4F4; margin-top: 2px;}
+            .friend_index{background: url(images/image-friend.png) no-repeat left top!important;}
+            /*            .community_index{background: url(images/image-community.png) no-repeat left top!important;}*/
+            #column1,#column2 {display: inline-block;width: 49.5%;vertical-align: top;}
+            #column1 {text-align: left;}.box_shadow8 {-webkit-box-shadow: 0 0 8px 0 #999;box-shadow: 0 0 8px 0 #999;}
+            .center_div { margin: 0px auto 0;} .width800{        width: 800px;} .clear {clear: both;} #nav2 {border-bottom: 1px solid #717373;} #nav2 div{} .index_fnx .fnx{text-align:center;font-size: 1em;font-weight: bold;} .fnx_detail{font-size: .85em;}#footer{    padding: 5px 10px 5px 10px; margin: 0 auto; } #footer a{    color:#333; padding: 0 .2em;} #footer a:hover{    color:#A6CC8B;} #footer li {    float: left;}</style>
     </head>
-    <body>
-        <form action="do_ajaxfileupload_post.php" method="POST" enctype="multipart/form-data">
-            <input type="file" name="fileToUpload"/>
-            <input type="submit"/>
-        </form>
-        <?php
-        $my_friends = getUserFriends($_SESSION['auth']['id'],true);
-        if(array_key_exists(50, $my_friends)){
-            echo 1;
-        }else{
-            echo 0;
-        }
-        echo json_encode($my_friends);
-//        $arr = search("Ah");
-//        if(isset($arr['people']))
-//        foreach ($arr['people'] as $x){
-//            echo "<img src='".$x['img']['image3535']."' />".$x['fullname']."<br/>";
-//        }
-//        if(isset($arr['community']))
-//        foreach ($arr['community'] as $x){
-//            echo "<img src='images/community_35x35.png' />".shortenStr($x['name'],50)."<br/>";
-//        }
-//        print_r($arr);
-        ?>
-<!--        <form action="do_ajaxfileupload_post.php" method="POST" enctype="multipart/form-data">
-            <input type="file" name="fileToUpload"/><input type="submit"/>
-        </form>
-        <div id="load">
-        </div>-->
-    </body>
+    <body> 
+        <div>
+            <div class="center_div width800">
+                <div class="inner_wrappper box_shadow8 center_div ">
+                    <div id="column1" style="width: 100%">
+                        <div  class="community_index index_fnx" > 
+                            <span> <h1 class="fnx"><img src="images/G.png" /> Ahmadu Bello University, Zaria<hr> </h1>
+                                <p class="fnx_detail"><img src="upload/thumbnails/1344921594_48_1342_100x100.jpeg" align="left"/><strong>Abdulrasheed Soladoye</strong><br/>Gossout Abdulkarim Rukayyahsincein King Fin Ahmadu Bello University, Zaria, NigeriaGossout Abdulrasheed SoladoyeGossout Abdulkarim Rukayyahsincein King Fin Ahmadu Bello University, Zaria, NigeriaGossout Abdulrasheed SoladoyeGossout Abdulkarim Rukayyahsincein King Fin Ahmadu Bello University, Zaria, NigeriaGossout Abdulrasheed SoladoyeGossout Abdulkarim Rukayyahsincein King Fin Ahmadu Bello University, Zaria, Nigeria</p>
+                            </span>
+                        </div>
+
+                    </div>
+                    <div id="footer" class="p_name">
+                        <hr>
+                        <table cellspacing="5px">
+                            <tr >
+                                <td colspan="3" style="font-size: 13px" font-family: 'Segoe UI',sans-serif;> This message was sent to soladnet@hotmail.com.
+                            </td>
+                        </tr>
+                    </table>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
 </html>
